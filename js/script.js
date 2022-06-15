9a05214bb90e1be2948e3ae5d8aac22d092adaf7
patyfil <patyfil@gmail.com>
2022-06-15 03:33:09 -0300

    alterando link da font

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-// const btnMobile = document.getElementById("btn-mobile");
+// ********************* scripts do menu sanduíche *********************
+const btnMobile = document.getElementById("btn-mobile");
 
-// function toggleMenu(event) {
-//   if (event.type === "touchstart") event.preventDefault();
-//   const nav = document.getElementById("nav");
-//   nav.classList.toggle("active");
-//   const active = nav.classList.contains("active");
-//   event.currentTarget.setAttribute("aria-expanded", active);
-//   if (active) {
-//     event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-//   } else {
-//     event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-//   }
-// }
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
 
-// btnMobile.addEventListener("click", toggleMenu);
-// btnMobile.addEventListener("touchstart", toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
-// ********************************************* scripts da galeria de fotos
+// ********************* scripts da galeria de fotos *********************
 
 var counter = 1;
 setInterval(function () {
@@ -25,4 +26,4 @@ setInterval(function () {
   if (counter > 4) {
     counter = 1;
   }
-}, 5000);
+}, 4000);
