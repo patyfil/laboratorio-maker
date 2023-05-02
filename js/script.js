853be94af917f9a7906060b3c5161a54746d14f6
patyfil <patyfil@gmail.com>
2023-05-01 21:24:32 -0300

    alt contato, css, js e resposta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // ********************* scripts do menu sanduíche *********************
+// constante para armazenar o valor
 const btnMobile = document.getElementById("btn-mobile");
 
+// função para ver se o menu foi apertado - se for apertado aparece o X e abre o menu
 function toggleMenu(event) {
   if (event.type === "touchstart") event.preventDefault();
   const nav = document.getElementById("nav");
@@ -18,7 +20,7 @@ btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
 
 // ********************* scripts da galeria de fotos *********************
-
+// variável para o contador que soma +1 a cada laço, se o contador for maior que 4, fecha o laço e retorna
 var counter = 1;
 setInterval(function () {
   document.getElementById("radio" + counter).checked = true;
@@ -27,3 +29,14 @@ setInterval(function () {
     counter = 1;
   }
 }, 4000);
+
+
+// ********* scripts para capturar o nome e imprimir na página resposta **********
+// essa não consegui fazer funcionar ainda prof rsrs, tenho que descobrir como imprimir em outro html
+var capturando = "";
+
+function capturar() {
+  capturando = document.getElementById('name').value;
+  document.getElementById('../resposta.html#valorDigitado').innerHTML = capturando;
+  localStorage.setItem('valorDigitado', capturando);
+}
